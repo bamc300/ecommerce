7da9487fbf11b31df816d1a71a3ecd3e96712c15
Bryan Alfonso Moncada contreras <bmoncada@asesoftware.com>
2021-11-24 19:43:00 -0500

    Servidor

---
+++ controller/auth.js
@@ -0,0 +1,142 @@
+const { toTitleCase, validateEmail } = require("../config/function");
+const bcrypt = require("bcryptjs");
+const userModel = require("../models/users");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/keys");
+
+class Auth {
+  async isAdmin(req, res) {
+    let { loggedInUserId } = req.body;
+    try {
+      let loggedInUserRole = await userModel.findById(loggedInUserId);
+      res.json({ role: loggedInUserRole.userRole });
+    } catch {
+      res.status(404);
+    }
+  }
+
+  async allUser(req, res) {
+    try {
+      let allUser = await userModel.find({});
+      res.json({ users: allUser });
+    } catch {
+      res.status(404);
+    }
+  }
+
+  /* User Registration/Signup controller  */
+  async postSignup(req, res) {
+    let { name, email, password, cPassword } = req.body;
+    let error = {};
+    if (!name || !email || !password || !cPassword) {
+      error = {
+        ...error,
+        name: "No puede estar vacio",
+        email: "No puede estar vacio",
+        password: "No puede estar vacio",
+        cPassword: "No puede estar vacio",
+      };
+      return res.json({ error });
+    }
+    if (name.length < 3 || name.length > 25) {
+      error = { ...error, name: "El nombre debe tener entre 3 y 25 caracteres" };
+      return res.json({ error });
+    } else {
+      if (validateEmail(email)) {
+        name = toTitleCase(name);
+        if ((password.length > 255) | (password.length < 8)) {
+          error = {
+            ...error,
+            password: "La contraseña debe tener 8 caracteres",
+            name: "",
+            email: "",
+          };
+          return res.json({ error });
+        } else {
+          // If Email & Number exists in Database then:
+          try {
+            password = bcrypt.hashSync(password, 10);
+            const data = await userModel.findOne({ email: email });
+            if (data) {
+              error = {
+                ...error,
+                password: "",
+                name: "",
+                email: "Correo ya existe",
+              };
+              return res.json({ error });
+            } else {
+              let newUser = new userModel({
+                name,
+                email,
+                password,
+                // ========= Here role 1 for admin signup role 0 for customer signup =========
+                userRole: 0, // Field Name change to userRole from role
+              });
+              newUser
+                .save()
+                .then((data) => {
+                  return res.json({
+                    success: "Cuenta creada con éxito. Por favor Iniciar sesión",
+                  });
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            }
+          } catch (err) {
+            console.log(err);
+          }
+        }
+      } else {
+        error = {
+          ...error,
+          password: "",
+          name: "",
+          email: "Correo no valido",
+        };
+        return res.json({ error });
+      }
+    }
+  }
+
+  /* User Login/Signin controller  */
+  async postSignin(req, res) {
+    let { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({
+        error: "Los campos no deben estar vacíos",
+      });
+    }
+    try {
+      const data = await userModel.findOne({ email: email });
+      if (!data) {
+        return res.json({
+          error: "Correo electrónico o contraseña no válidos",
+        });
+      } else {
+        const login = await bcrypt.compare(password, data.password);
+        if (login) {
+          const token = jwt.sign(
+            { _id: data._id, role: data.userRole },
+            JWT_SECRET
+          );
+          const encode = jwt.verify(token, JWT_SECRET);
+          return res.json({
+            token: token,
+            user: encode,
+          });
+        } else {
+          return res.json({
+            error: "Correo electrónico o contraseña no válidos",
+          });
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+const authController = new Auth();
+module.exports = authController;
